f57c319f90d8f310d2b4dfb9d0f586d002c2b7fa
Kovács Ádám <the.adam.kovacs@gmail.com>
2018-08-27 16:25:47 +0200

    Added event listeners to open each different activities on click.

--- app/src/main/java/com/example/android/miwok/MainActivity.java
+++ app/src/main/java/com/example/android/miwok/MainActivity.java
@@ -19,6 +19,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -28,10 +29,68 @@ public class MainActivity extends AppCompatActivity {
 
         // Set the content of the activity to use the activity_main.xml layout file
         setContentView(R.layout.activity_main);
+
+        //Find the "numbers' TextView by id
+        View numbers = findViewById(R.id.numbers);
+        //Attaching a listener to the "numbers" TextView while creating a new object instance of an OnClickListener
+        numbers.setOnClickListener(new View.OnClickListener(){
+
+            //Setting what the freshly created event listener
+            @Override
+            public void onClick(View view) {
+                //Creating a new intent to open the NumbersActivity
+                Intent openNumbersActivity = new Intent(MainActivity.this, NumbersActivity.class);
+                //Starting the new activity
+                startActivity(openNumbersActivity);
+            }
+        });
+
+
+        //Find the "colors' TextView by id
+        View colors = findViewById(R.id.colors);
+        //Attaching a listener to the "numbers" TextView while creating a new object instance of an OnClickListener
+        colors.setOnClickListener(new View.OnClickListener(){
+
+            //Setting what the freshly created event listener
+            @Override
+            public void onClick(View view) {
+                //Creating a new intent to open the NumbersActivity
+                Intent openColorsActivity = new Intent(MainActivity.this, ColorsActivity.class);
+                //Starting the new activity
+                startActivity(openColorsActivity);
+            }
+        });
+
+
+        //Find the "family' TextView by id
+        View family = findViewById(R.id.family);
+        //Attaching a listener to the "numbers" TextView while creating a new object instance of an OnClickListener
+        family.setOnClickListener(new View.OnClickListener(){
+
+            //Setting what the freshly created event listener
+            @Override
+            public void onClick(View view) {
+                //Creating a new intent to open the NumbersActivity
+                Intent openFamilyActivity = new Intent(MainActivity.this, FamilyActivity.class);
+                //Starting the new activity
+                startActivity(openFamilyActivity);
+            }
+        });
+
+        //Find the "phrases' TextView by id
+        View phrases = findViewById(R.id.phrases);
+        //Attaching a listener to the "numbers" TextView while creating a new object instance of an OnClickListener
+        phrases.setOnClickListener(new View.OnClickListener(){
+
+            //Setting what the freshly created event listener
+            @Override
+            public void onClick(View view) {
+                //Creating a new intent to open the NumbersActivity
+                Intent openPhrasesActivity = new Intent(MainActivity.this, PhrasesActivity.class);
+                //Starting the new activity
+                startActivity(openPhrasesActivity);
+            }
+        });
     }
 
-    public void openNumbersList(View view) {
-        Intent openNumbersActivity = new Intent(this, NumbersActivity.class);
-        startActivity(openNumbersActivity);
-    }
 }
